00f2c5455c9a0eafc5a234a004875c9f4ee99c21
BowTiedSwan <86532747+BowTiedSwan@users.noreply.github.com>
2022-11-15 09:41:02 +0000

    Initial commit

---
+++ src/config/unlock.ts
@@ -0,0 +1,12 @@
+export const paywallConfig: Record<string, unknown> = {
+  locks: {
+    "0x77d11b698808eef99e32d3273dc88cc34097786f": {
+      network: 5,
+      name: "wonderwomancode",
+    },
+  },
+  pessimistic: false,
+  title: "Super Hero Zone",
+  icon: "https://pin.ski/3Fc5Dqa",
+  persistentCheckout: false,
+};
